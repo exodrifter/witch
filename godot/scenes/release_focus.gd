10b17d125dbe64ac23985c9cc3ff1f40402abf18
Ava Pek <code@exodrifter.space>
2024-01-23 12:52:43 -0600

    Hide note when empty

--- godot/scenes/release_focus.gd
+++ godot/scenes/release_focus.gd
@@ -1,5 +1,12 @@
 extends TextEdit
 
+@onready var note = $"../../.."
+
 func _process(_delta):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		release_focus()
+
+	if text == "":
+		note.self_modulate = Color.TRANSPARENT
+	else:
+		note.self_modulate = Color.WHITE
